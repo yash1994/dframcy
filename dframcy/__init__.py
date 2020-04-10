c25cb7bdbbef0e3ad5bffaffdd12257b31b470cf
yash <patadiayash@gmail.com>
2020-04-10 12:03:22 +0530

    update version in init

--- dframcy/__init__.py
+++ dframcy/__init__.py
@@ -1,5 +1,5 @@
 from dframcy.dframcy import DframCy
 from dframcy.matcher import DframCyMatcher, DframCyPhraseMatcher
 
-__version__ = "0.0.1"
+__version__ = "0.1.4"
 __all__ = ["DframCy", "DframCyMatcher", "DframCyPhraseMatcher"]
